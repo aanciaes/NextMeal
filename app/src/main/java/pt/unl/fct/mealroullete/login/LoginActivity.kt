a518b70983663bb5e48fa401f4067c1281b123f2
aanciaes <aanciaes.miguel@gmail.com>
2018-10-22 22:43:21 +0100

    Login Activity

---
+++ app/src/main/java/pt/unl/fct/mealroullete/login/LoginActivity.kt
@@ -0,0 +1,90 @@
+package pt.unl.fct.mealroullete.login
+
+import android.app.Dialog
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.CardView
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.*
+import pt.unl.fct.mealroullete.R
+import pt.unl.fct.mealroullete.register.RegisterActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setMainListeners()
+    }
+
+    // Set all listeners for this activity
+    private fun setMainListeners() {
+        onClickShowPassword()
+        loginWithFingerprint();
+        noAccountYetRegister()
+        login()
+    }
+
+    // Changes between hidden or clear text password
+    private fun onClickShowPassword() {
+        val showHidePassword = findViewById<ImageButton>(R.id.showHidePassword)
+        showHidePassword.setOnClickListener {
+            val passwordField = findViewById<EditText>(R.id.passwordField)
+
+            if (passwordField.transformationMethod is PasswordTransformationMethod) {
+                passwordField.transformationMethod = HideReturnsTransformationMethod.getInstance();
+                showHidePassword.setImageResource(R.drawable.ic_action_hide_password)
+            } else {
+                passwordField.transformationMethod = PasswordTransformationMethod.getInstance();
+                showHidePassword.setImageResource(R.drawable.ic_action_show_password)
+            }
+        }
+    }
+
+    // Shows fingerprint dialog
+    private fun showFingerprintLogin() {
+        val dialog = Dialog(this);
+        dialog.setContentView(R.layout.fingerprint_dialog_content);
+
+        val usePassword = dialog.findViewById<Button>(R.id.usePasswordButton)
+        usePassword.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+
+    // Changes from password authentication to fingerprint authentication
+    private fun loginWithFingerprint() {
+        val loginWithFingerPrint = findViewById<ImageButton>(R.id.fingerprintLogin)
+        loginWithFingerPrint.setOnClickListener {
+            showFingerprintLogin()
+        }
+    }
+
+    // Jumps to register activity
+    private fun noAccountYetRegister() {
+        val registerJump = findViewById<TextView>(R.id.registerText)
+        registerJump.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    // Performs simple username/password authentication
+    // TODO: jump to home page
+    private fun login() {
+        val login = findViewById<CardView>(R.id.loginButtonCardView)
+        login.setOnClickListener {
+            val username = findViewById<EditText>(R.id.usernameField)
+            val password = findViewById<EditText>(R.id.passwordField)
+
+            if (username.text.toString() == "manel" && password.text.toString() == "sougay") {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Wrong username or password", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/pt/unl/fct/mealroullete/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package pt.unl.fct.mealroullete.register
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import pt.unl.fct.mealroullete.R
+
+class RegisterActivity: AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_activity)
+    }
+}
