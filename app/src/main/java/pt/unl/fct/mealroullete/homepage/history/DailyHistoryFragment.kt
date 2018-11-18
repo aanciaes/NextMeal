65e58a0cb0639bfac68f4f5af13042ec529e2eba
rmamaral <rm.amaral@campus.fct.unl.pt>
2018-11-18 02:07:41 +0000

    history hardcoded

--- app/src/main/java/pt/unl/fct/mealroullete/homepage/history/DailyHistoryFragment.kt
+++ app/src/main/java/pt/unl/fct/mealroullete/homepage/history/DailyHistoryFragment.kt
@@ -1,15 +1,30 @@
 package pt.unl.fct.mealroullete.homepage.history
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import pt.unl.fct.mealroullete.R
 
 class DailyHistoryFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_daily_history, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_daily_history, container, false)
+        val container = view.findViewById<TableLayout>(R.id.ingredient_table)
+
+        var x = 1
+        while(x < 4){
+            val child = inflater.inflate(R.layout.table_item_history, container, false) as LinearLayout
+            child.findViewById<TextView>(R.id.historyName).text = "Dish Name" + x
+
+            container.addView(child)
+            x++
+        }
+        return view
     }
 }
