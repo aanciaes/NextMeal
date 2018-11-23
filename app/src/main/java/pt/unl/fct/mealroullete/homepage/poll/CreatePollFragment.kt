e0f3a10a78e354799cb9769469f1db2e7cbca1eb
joaoreis <jc.reis@campus.fct.unl.pt>
2018-11-23 19:45:52 +0000

    CreatePoll page

--- app/src/main/java/pt/unl/fct/mealroullete/homepage/poll/CreatePollFragment.kt
+++ app/src/main/java/pt/unl/fct/mealroullete/homepage/poll/CreatePollFragment.kt
@@ -5,11 +5,26 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TableLayout
+import android.widget.TextView
 import pt.unl.fct.mealroullete.R
 
 class CreatePollFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_create_poll, container, false)
+        val view = inflater.inflate(R.layout.fragment_create_poll, container, false)
+
+
+        val container = view.findViewById<TableLayout>(R.id.ingredient_table)
+        var x = 1
+        while (x < 8) {
+            val child = inflater.inflate(R.layout.table_item_polluser, container, false) as LinearLayout
+            child.findViewById<TextView>(R.id.userEmail).text = "email@email.com " + x.toString()
+            container.addView(child)
+            x++
+        }
+
+        return view
     }
 }
